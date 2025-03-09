267406b2bc139d073f07f5bd4a2f993fd799baad
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-03-10 02:28:08 +0530

    nothing is working and i am tired

--- BACKEND/server.js
+++ BACKEND/server.js
@@ -2,15 +2,22 @@
 const express = require('express');
 const cors =require('cors');
 const connectdb = require("./config/db");
-const registerroute = require("./controllers/registeruser");
+const registerroute = require("./routes/registeruser");
 //initiaze express
 const app = express();
 //middlewares
-app.use(express.json);
+app.use(express.urlencoded({ extended: true }));
 app.use(cors);
+app.use(express.json);
 //connect to db
-connectdb();
+//connectdb();
 //routes
 app.use("/api/register",registerroute);
+//testing 
+app.get("/test", (req, res) => {
+    res.send("✅ Test route is working!");
+    console.log("get request here")
+  });
+  
 //server 
-app.listen(5000,console.log("server is running"));
+app.listen(6000,console.log("server is running"));
