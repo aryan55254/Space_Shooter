0e17a84ed6872872efea8a851b3a3212e77d310a
aryanmishra55254 <aryanmishra55254@gmail.com>
2025-03-12 13:23:34 +0530

    rewrite whole thing again

--- BACKEND/utility/jwt.js
+++
@@ -1,6 +0,0 @@
-const jwt = require("jsonwebtoken");
-require("dotenv").config();
-const generatetoken = (userid) => {
-  return jwt.sign({ userid }, process.env.jwt_secret, { expiresIn: "15m" }
-  )};
-  module.exports = {generatetoken};
